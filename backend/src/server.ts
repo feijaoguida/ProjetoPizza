f923ebda1e8d91836def2420adb2d2561f0f4914
Roseweltty Guida <roseweltty.guida@squadra.com.br>
2022-11-10 22:02:56 -0300

    chore: configuração swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,21 @@ import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { router } from './router'
+import swaggerUi  from 'swagger-ui-express'
 
 const app = express();
 app.use(express.json())
 app.use(cors())
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    }
+  })
+)
 
 app.use(router);
 

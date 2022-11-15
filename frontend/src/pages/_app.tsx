19fa7a9ddacd540cb8e256e149b8c9b611e40f50
Roseweltty Guida <roseweltty.guida@squadra.com.br>
2022-11-15 19:30:54 -0300

    feat: context auth and page login

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,8 +2,14 @@ import '../../styles/globals.scss'
 
 import type { AppProps } from 'next/app'
 
+import { AuthProvider } from '../contexts/AuthContext'
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default App
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,30 @@
+import { FormEvent, useContext, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from '../../styles/Home.module.scss'
 import logoImg from '../../public/logo.svg'
 import { Input } from '../components/ui/input'
 import { Button } from '../components/ui/Button'
-import Link from 'next/link'
+
+import { AuthContext } from '../contexts/AuthContext'
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false)
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
   return (
     <>
       <Head>
@@ -17,20 +34,25 @@ export default function Home() {
         <Image src={logoImg} alt="Logo Pizzaria" />
 
         <div className={styles.login}>
-          <form >
+          <form onSubmit={handleLogin}>
             <Input
               placeholder='Digite seu email'
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <Input
               placeholder='Digite sua senha'
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <Button
               type="submit"
-              loading={false}
+              loading={loading}
+              
             >
               Acessar
             </Button>
